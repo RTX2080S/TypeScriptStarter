b3389d823e0b3ec6b74f1003e3934c427fdcb7d8
LL <52593730@qq.com>
2017-08-02 21:56:45 +1200

    Add TypeScript stuff

---
+++ TypeScriptStarter/Controllers/MealsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TypeScriptStarter.Controllers
+{
+    [Authorize]
+    public class MealsController : Controller
+    {
+        // GET: Meals
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TypeScriptStarter/ViewModels/Meal.ts
@@ -0,0 +1,14 @@
+﻿
+export class Meal {
+
+    public id: KnockoutObservable<number>;
+    public mealName: KnockoutObservable<string>;
+    public price: KnockoutObservable<any>;
+
+    constructor(data: any) {
+        console.log(data);
+        this.id = ko.observable(data.Id);
+        this.mealName = ko.observable(data.Name);
+        this.price = ko.observable(data.Price);
+    }
+}
