374237b7a6c28d54a00517a1d06177544f5e498c
LL <52593730@qq.com>
2017-08-19 00:55:44 +1200

    use web api, no compromise!

--- TypeScriptStarter/Controllers/MealsController.cs
+++ TypeScriptStarter/Controllers/MealsController.cs
@@ -15,28 +15,5 @@ namespace TypeScriptStarter.Controllers
         {
             return View();
         }
-
-        Meal[] products = new Meal[]
-        {
-            new Meal { Id = 0, Name = "Standard (sandwich)", Price = 0 },
-            new Meal { Id = 1, Name = "Premium (lobster)", Price = 34.95M },
-            new Meal { Id = 2, Name = "Ultimate (whole zebra)", Price = 290M }
-        };
-
-        public JsonResult GetAllMeals()
-        {
-            var username = User.Identity.Name;
-            return Json(products, JsonRequestBehavior.AllowGet);
-        }
-
-        public JsonResult GetMeal(int id)
-        {
-            var product = products.FirstOrDefault((p) => p.Id == id);
-            if (product == null)
-            {
-                return Json(null);
-            }
-            return Json(product, JsonRequestBehavior.AllowGet);
-        }
     }
 }
---
+++ TypeScriptStarter/Controllers/MealsDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TypeScriptStarter.Models;
+
+namespace TypeScriptStarter.Controllers
+{
+    [Authorize]
+    public class MealsDataController : ApiController
+    {
+        Meal[] products = new Meal[]
+        {
+            new Meal { Id = 0, Name = "Standard (sandwich)", Price = 0 },
+            new Meal { Id = 1, Name = "Premium (lobster)", Price = 34.95M },
+            new Meal { Id = 2, Name = "Ultimate (whole zebra)", Price = 290M }
+        };
+
+        public IHttpActionResult GetAllMeals()
+        {
+            var username = User.Identity.Name;
+            return Json(products);
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetMeal(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return Json(string.Empty);
+            }
+            return Json(product);
+        }
+    }
+}
